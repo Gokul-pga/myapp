fc9f82e054695ea0a41ca3d07e57d935c9dfe1c3
Gokul-pga <gokulanand2508@gmail.com>
2023-04-26 20:51:40 +0530

    remove arrayApi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Website from './layout/Website';
 import Loginpage from './pages/Loginpage';
 import TestAPI from './TestAPI';
-import ArrayAPI from '../src/ArrayAPI/ArrayAPI';
 import ToDolist from './todolist/ToDolist';
 import DefaultPage from './DefaultPage';
 
@@ -20,7 +19,6 @@ function App({pageProps}) {
     <Route path='/Loginpage' element={<Loginpage/>} />
       <Route path='/Website' element={<Website/>} />
       <Route path='/testapi' element={<TestAPI/>} />
-      <Route path='/arrayapi' element={<ArrayAPI/>} />
       <Route path='/ToDolist' element={<ToDolist/>} />
 
     </Routes>
